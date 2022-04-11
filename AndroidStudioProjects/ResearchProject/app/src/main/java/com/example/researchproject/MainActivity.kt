d1c8de474c38efb1a051268efaef43fbb3b8e28c
Kris Simpson <krissimpson@Kriss-MacBook-Pro.local>
2022-04-11 21:03:01 +1000

    added android projects

---
+++ AndroidStudioProjects/ResearchProject/app/src/main/java/com/example/researchproject/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.researchproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import org.opencv.android.JavaCamera2View
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # DSRUStructureFromMotion
 Repo to hold files for the DSRU Summer Research project
+
+This repo includes:
+
+1) Attempted Android Studio projects cloned from github resources
+2) Final docker image with working web interface to produce 3D point cloud for measurement
+3) Test project files
